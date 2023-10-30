9514a1c4660d2436cb329b2f3d99086a5b206ae6
Majd alali <m-syr@outlook.sa>
2023-10-30 16:10:15 +0800

    TEST

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 
+// END POINTS FOR USER ROUTES
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 router.get("/users", userController.getAllUsers);
